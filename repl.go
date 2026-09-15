84d8c556faa01899c570a0cb718b510f5e6ad4f1
ItencY <itency@outlook.com>
2026-09-15 20:50:58 +0300

    feat: implement REPL functionality and add .gitignore

--- repl.go
+++ repl.go
@@ -1,8 +1,23 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		input := scanner.Text()
+		firstWord := strings.Split(input, " ")[0]
+		loweredFirstWord := strings.ToLower(firstWord)
+		fmt.Printf("Your command was: %s\n", loweredFirstWord)
+	}
+}
 
 func cleanInput(text string) []string {
 	cleanText := []string{}
