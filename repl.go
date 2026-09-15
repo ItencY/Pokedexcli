746cf9a2172d052095c17cb3bf9b1f38d6ecf32c
ItencY <itency@outlook.com>
2026-09-15 21:24:31 +0300

    feat: implement command handling with exit and help commands

--- repl.go
+++ repl.go
@@ -7,6 +7,12 @@ import (
 	"strings"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
 func startRepl() {
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
@@ -15,9 +21,14 @@ func startRepl() {
 			break
 		}
 		input := scanner.Text()
-		firstWord := strings.Split(input, " ")[0]
-		loweredFirstWord := strings.ToLower(firstWord)
-		fmt.Printf("Your command was: %s\n", loweredFirstWord)
+		cmd, ok := getCommands()[cleanInput(input)[0]]
+		if !ok {
+			fmt.Println("Unknown command")
+			continue
+		}
+		if err := cmd.callback(); err != nil {
+			fmt.Println("Error executing command:", err)
+		}
 	}
 	if err := scanner.Err(); err != nil {
 		fmt.Fprintln(os.Stderr, "reading input:", err)
@@ -34,4 +45,19 @@ func cleanInput(text string) []string {
 		}
 	}
 	return cleanText
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+	}
 }
